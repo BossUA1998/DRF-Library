ced61e560aa03c9e197a18aa8a1808908f2f665c
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2026-01-23 11:54:18 +0200

    Book model has been developed

--- library/models.py
+++ library/models.py
@@ -1,3 +1,15 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.text import gettext_lazy as _
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD", _("Hard")
+        SOFT = "SOFT", _("Soft")
+
+    title = models.CharField(max_length=100, unique=True)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(max_length=10, choices=CoverChoices.choices)
+    inventory = models.PositiveIntegerField(default=0)
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
