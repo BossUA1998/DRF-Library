a08a58060c5d9bbbab2a3f056e0a46089abedba2
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2026-01-26 14:15:20 +0200

    Created payments app

--- library_service/settings.py
+++ library_service/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # apps
     "library",
     "user",
     "borrowings",
+    "payments",
+    # ---
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
